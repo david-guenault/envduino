101514020948b05760c64daaee8f163db50a121f
David GUENAULT <dguenault@monitoring-fr.org>
2013-07-04 07:50:22 +0200

    Added webservice skeleton

---
+++ tools/webservice.py
@@ -0,0 +1,22 @@
+from bottle import route, run, response
+import json
+import os
+
+
+@route('/get/<name>',method="GET")
+def wget(name=''):
+    response.content_type = 'application/json'
+    return json.dumps({ "success" : False, "paths" : [], "error" : "not implemented yet" })
+
+@route('/put/<name>',method="GET")
+def wput(name=''):
+    response.content_type = 'application/json'
+    return json.dumps({ "success" : False, "paths" : [], "error" : "not implemented yet" })
+
+@route('/delete/<name>',method="GET")
+def wdelete(name=''):
+    response.content_type = 'application/json'
+    return json.dumps({ "success" : False, "paths" : [], "error" : "not implemented yet" })
+
+
+run(host='localhost', port=8181)
